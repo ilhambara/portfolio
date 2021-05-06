c6bbc25db268c76a5f009c0b301d87180b2131b6
ilhambara <g2a.ilhammubarok@gmail.com>
2021-05-06 18:01:00 +0700

    Fix: Build error occured

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -1,46 +1,42 @@
-// import styles from '../../styles/Blog.module.css';
+import styles from '../../styles/Blog.module.css';
 
-// export const getStaticPaths = async () => {
-//   const res = await fetch('https://dev.to/api/articles?username=ilhambara');
-//   const data = await res.json();
+export const getStaticPaths = async () => {
+  const res = await fetch('https://dev.to/api/articles?username=ilhambara');
+  const data = await res.json();
 
-//   const paths = data.map((post) => {
-//     return {
-//       params: { id: post.id.toString() },
-//     };
-//   });
+  const paths = data.map((post) => {
+    return {
+      params: { id: post.id.toString() },
+    };
+  });
 
-//   return {
-//     paths,
-//     fallback: false,
-//   };
-// };
-
-// export const getStaticProps = async (context) => {
-//   const id = context.params.id;
-//   const res = await fetch('https://dev.to/api/articles/' + id);
-//   const data = await res.json();
-
-//   return {
-//     props: { post: data },
-//   };
-// };
-
-// const Posts = ({ post }) => {
-//   return (
-//     <main className={styles.main}>
-//       <div>
-//         <h3>{post.title}</h3>
-//         <p>{post.body_markdown}</p>
-//       </div>
-//     </main>
-//   );
-// };
-
-// export default Posts;
-
-import React from 'react';
-
-export const Posts = () => {
-  return <div></div>;
+  return {
+    paths,
+    fallback: false,
+  };
 };
+
+export const getStaticProps = async (context) => {
+  const id = context.params.id;
+  const res = await fetch('https://dev.to/api/articles/' + id);
+  const data = await res.json();
+
+  return {
+    props: { post: data },
+  };
+};
+
+const Posts = ({ post }) => {
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <div className={styles.posts}>
+          <h3>{post.title}</h3>
+          <p>{post.body_html}</p>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default Posts;
