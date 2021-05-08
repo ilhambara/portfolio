9a68fba80199e985872c9d62bf5813e91698ac14
ilhambara <g2a.ilhammubarok@gmail.com>
2021-05-09 02:24:48 +0700

    Edit: social meta tags(1)

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,9 @@ export default function Home() {
         <meta charSet="utf-8" />
         <meta property="og:url" content={'https://ilhambara.vercel.app/'} key="ogurl" />
         <meta property="og:image" content={'https://res.cloudinary.com/drixfgugx/image/upload/v1620498970/web-v3/og-image_zzoxrr.png'} key="ogimage" />
+        <meta property="og:image:type" content="website" />
+        <meta property="og:image:width" content="1920" />
+        <meta property="og:image:height" content="1080" />
         <meta property="og:site_name" content={'Ilham Bara | Next Portfolio'} key="ogsitename" />
         <meta property="og:title" content={'Portfolio Website'} key="ogtitle" />
         <meta
@@ -21,6 +24,9 @@ export default function Home() {
           }
           key="ogdesc"
         />
+        <meta name="twitter:title" content={'Ilham Bara | Next Portfolio'} />
+        <meta name="twitter:description" content={'Ilham Bara is a passionate and curious web developer. Doing anything related to web dev things.'} />
+        <meta name="twitter:image" content={'https://res.cloudinary.com/drixfgugx/image/upload/v1620498970/web-v3/og-image_zzoxrr.png'} />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content={'@zgve679jdv'} />
         <meta name="twitter:creator" content={'@zgve679jdv'} />
