a895b814c8a42816186158a46470e74e9c11546e
ilhambara <g2a.ilhammubarok@gmail.com>
2021-05-09 03:05:19 +0700

    Edit: social meta tags(5)

--- pages/index.js
+++ pages/index.js
@@ -10,18 +10,16 @@ export default function Home() {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="utf-8" />
-        <meta property="description" content={'Ilham Bara is a passionate and curious web developer. Doing anything related to web dev things.'} />
+        <meta content="ie=edge" http-equiv="X-UA-Compatible" />
+        <meta name="description" content={'Ilham Bara is a passionate and curious web developer. Doing anything related to web dev things.'} />
         <meta property="og:url" content={'https://ilhambara.vercel.app/'} key="ogurl" />
-        <meta property="og:image" content={'/img/og-image.png'} key="ogimage" />
-        <meta property="og:image:type" content="website" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={'Portfolio Website'} key="ogtitle" />
+        <meta property="og:site_name" content={'Ilham Bara | Next Portfolio'} key="ogsitename" />
+        <meta property="og:description" content={'Ilham Bara is a passionate and curious web developer. Doing anything related to web dev things.'} key="ogdesc" />
+        <meta property="og:image" content={'https://res.cloudinary.com/drixfgugx/image/upload/v1620503805/web-v3/og-image_wrteqi.png'} />
         <meta property="og:image:width" content="1920" />
         <meta property="og:image:height" content="1080" />
-        <meta property="og:site_name" content={'Ilham Bara | Next Portfolio'} key="ogsitename" />
-        <meta property="og:title" content={'Portfolio Website'} key="ogtitle" />
-        <meta property="og:description" content={'Ilham Bara is a passionate and curious web developer. Doing anything related to web dev things.'} key="ogdesc" />
-        <meta name="twitter:title" content={'Ilham Bara | Next Portfolio'} />
-        <meta name="twitter:description" content={'Ilham Bara is a passionate and curious web developer. Doing anything related to web dev things.'} />
-        <meta name="twitter:image" content={'/img/og-image.png'} />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content={'@zgve679jdv'} />
         <meta name="twitter:creator" content={'@zgve679jdv'} />
