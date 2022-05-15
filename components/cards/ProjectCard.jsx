9931c1eb900f888fd3f6e9abedaae28d0f0efbf8
ilhambara <g2a.ilhammubarok@gmail.com>
2022-05-16 04:14:47 +0700

    update: placeholder content for projects (2)

--- components/cards/ProjectCard.jsx
+++ components/cards/ProjectCard.jsx
@@ -1,11 +1,14 @@
 import NextImage from "next/image";
-import { ButtonGroup, Flex, Heading, IconButton, Stack, Text, useColorModeValue, VStack } from "@chakra-ui/react";
+import { Box, ButtonGroup, Flex, Heading, IconButton, Stack, Text, useColorModeValue, VStack } from "@chakra-ui/react";
 import { VscGithub, VscRocket } from "react-icons/vsc";
 
 export const ProjectCard = () => {
   const width = 480;
-  const height = 300;
+  const height = 280;
   // const picsum = `https://picsum.photos/${width}/${height}`;
+
+  const bgImg = useColorModeValue("gray.400", "gray.700");
+  const bgImgHover = useColorModeValue("gray.300", "gray.800");
 
   const bgColor = useColorModeValue("gray.200", "#12151d");
   const linkColor = useColorModeValue("blue.600", "blue.300");
@@ -13,18 +16,18 @@ export const ProjectCard = () => {
   const fakeData = [
     {
       title: "Cross-platform App Boilerplate",
-      desc: "This project is a starter template to develop desktop-based app using Electron that combined with React js and TailwindCSS. Configured for Windows, MacOS, and (some) Linux.",
-      img: `https://picsum.photos/id/20/${width}/${height}`,
+      desc: "This project is a starter template to develop desktop-based app using Electron that combined with React js and TailwindCSS. Configured for Windows, MacOS, and (some) Linux. Electron that combined with React js.",
+      img: "/img/thumb/thumb1.png",
+    },
+    {
+      title: "spotify-play-next.vercel.app",
+      desc: "Display 'Currently Playing Song' Status From Spotify API using Next.js. Project details and explanations are not available yet. Planned to working on it soon! 👀⏳",
+      img: `/img/thumb/thumb2.png`,
     },
     {
       title: "Mortimer Community",
-      desc: "An online based community website created in a combination of React Js and Contentful.",
-      img: `https://picsum.photos/id/1000/${width}/${height}`,
-    },
-    {
-      title: "Use Google Sheets for Database",
-      desc: "This project is a quick proof-of-concept in turned google spreadsheets into a database using provided API from its official source Google Cloud Platform (GCP).",
-      img: `https://picsum.photos/id/894/${width}/${height}`,
+      desc: "An online based community website created in a combination of React Js and Contentful. Bootstrapped with Create React App.",
+      img: `/img/thumb/thumb3.png`,
     },
   ];
 
@@ -36,39 +39,55 @@ export const ProjectCard = () => {
             key={data.title}
             direction={["column", "row"]}
             w="full"
-            minH="10rem"
+            minH="300px"
             bgColor={bgColor}
             borderRadius={4}
-            p={[4, 2]}
-            spacing={[6, 2]}
+            spacing={2}
           >
-            <Flex w={["full", "50%"]}>
-              <NextImage
-                width={width}
-                height={height}
-                objectFit="contain"
-                src={data.img}
-                alt="project thumbnail"
-                blurDataURL={data.img}
-                placeholder="blur"
-              />
+            <Flex
+              w={["full", "50%"]}
+              justify="center"
+              align="end"
+              bgColor={bgImg}
+              overflow="hidden"
+              transition="0.3s"
+              _hover={{ bgColor: bgImgHover }}
+            >
+              <Flex
+                w="94%"
+                h="fit-content"
+                justify="end"
+                align="end"
+                pos="relative"
+                bottom="-16px"
+                boxShadow="xl"
+                transition="0.3s"
+                borderRadius={4}
+                overflow="hidden"
+                _hover={{ transform: `translate(0, -8px)`, boxShadow: "md", cursor: "pointer" }}
+              >
+                <NextImage
+                  width={width}
+                  height={height}
+                  objectFit="contain"
+                  src={data.img}
+                  alt="project thumbnail"
+                  blurDataURL={data.img}
+                  placeholder="blur"
+                />
+              </Flex>
             </Flex>
 
-            <VStack w={["full", "50%"]} align="start" justify="space-between" p={[0, 4]} spacing={8}>
-              <VStack minH={["fit-content", 200]} align="start" spacing={[4, 8]} lineHeight={1.8}>
-                <Heading
-                  as="h3"
-                  fontSize={["2xl", "3xl"]}
-                  color={linkColor}
-                  _hover={{ textDecoration: "underline", cursor: "pointer" }}
-                >
+            <VStack w={["full", "50%"]} align="start" justify="space-between" p={[3, 6]} spacing={[8, 4]}>
+              <VStack align="start" spacing={[4, 6]} lineHeight={1.8}>
+                <Heading as="h3" fontSize={["xl", "2xl"]} lineHeight={1.6} color={linkColor}>
                   {data.title}
                 </Heading>
 
                 <Text opacity={0.75}>{data.desc}</Text>
               </VStack>
 
-              <ButtonGroup w="full" justifyContent="end" variant="ghost" spacing={2}>
+              <ButtonGroup w="full" justifyContent="end" variant="ghost" spacing={2} pb={[4, 0]}>
                 <IconButton colorScheme="gray" aria-label="Preview Button" icon={<VscGithub size={24} />} />
                 <IconButton colorScheme="blue" aria-label="Preview Button" icon={<VscRocket size={24} />} />
               </ButtonGroup>
